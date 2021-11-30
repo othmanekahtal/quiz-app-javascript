89551c452109df78a8b7f5be4fe0ba3d0f877f26
othmane <othmanekahtal@gmail.com>
2021-11-30 21:06:38 +0100

    buid panel for coatch

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ['./*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        'dashboard-column-guide':"260px 1fr",
+      },
+      gridTemplateRows:{
+        'dashboard-row-guide':"100px calc(100vh - 100px)",
+      }
+    },
   },
   variants: {
     extend: {
